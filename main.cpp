b2bc87984e472cc9bbd81b430a1e17eb062c9a0b
katajima <k023g1078@g.neec.ac.jp>
2024-09-30 10:27:44 +0900

    一旦

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <iostream>
+#include <Windows.h>
 
 
 int main() {
+	SetConsoleOutputCP(65001);
 
-	printf("LE2A_09_カタジマ_ロウノシン_PG3");
+	printf("LE2A_09_繧ｫ繧ｿ繧ｸ繝枩繝ｭ繧ｦ繝弱す繝ｳ_PG3");
 
 	return 0;
 }
