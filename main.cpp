dd5f4f704edf82d5f0165e12159bb44137291816
katajima <k023g1078@g.neec.ac.jp>
2024-11-13 22:15:52 +0900

    動物

--- main.cpp
+++ main.cpp
@@ -3,9 +3,70 @@
 #include <Windows.h>
 
 
+// 基底クラス
+class Animal {
+public:
+
+	// 鳴く
+	virtual void CryVoice();
+
+protected:
+
+};
+
+// 派生クラス
+class Cat : public Animal
+{
+public:
+
+	void CryVoice() override;
+
+
+private:
+
+};
+
+// 派生クラス 
+class Dog : public Animal
+{
+public:
+
+	void CryVoice() override;
+
+
+private:
+
+};
+
+
+
 int main() {
-	printf("LE2A_09_カタジマ_ロウノシン_PG3");
+	
+	Cat cat;
+	cat.CryVoice();
+	Dog dog;
+	dog.CryVoice();
+
 
 	return 0;
 }
 
+
+
+
+void Cat::CryVoice()
+{
+	printf("ニャー!!\n");
+}
+
+
+void Dog::CryVoice()
+{
+	printf("ワン!!\n");
+}
+
+
+
+void Animal::CryVoice()
+{
+}
