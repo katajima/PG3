8fe9c9654602869eee1a8675aeddbabb4d4ed564
katajima <k023g1078@g.neec.ac.jp>
2024-11-29 22:08:22 +0900

    ファイル

--- main.cpp
+++ main.cpp
@@ -1,11 +1,42 @@
-#include <stdio.h>
 #include <iostream>
-#include <Windows.h>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
 
 
 int main() {
-	printf("LE2A_09_カタジマ_ロウノシン_PG3");
 
-	return 0;
+
+    // ファイル名(PG3_2024_03_02の中身のかっこの部分が邪魔だから消して.txt)
+    const std::string fileName = "PG3_2024_03_02.txt";
+    // ファイルを開く
+    std::ifstream input(fileName);
+    // メールアドレスを格納するvector
+    std::vector<std::string> emVector;
+    std::string em;
+
+
+
+    // ファイルからデータを読み込む
+    while (std::getline(input, em, ',')) {
+        emVector.push_back(em);
+    }
+    // くろーず
+    input.close();
+
+    // 学籍番号順にソート
+    std::sort(emVector.begin(), emVector.end(), [](const std::string& a, const std::string& b) {return a < b;});
+
+
+    // 結果を表示
+    std::cout << "学籍番号順リスト:\n";
+
+
+    for (const auto& e : emVector) {
+        std::cout << e << std::endl;
+    }
+
+    return 0;
 }
-
